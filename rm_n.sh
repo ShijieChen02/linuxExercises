bf1f90793eabff1adf13fcc63a9b129491abcb33
Fuxin Wang <fwang297@wisc.edu>
2023-10-17 15:20:28 -0500

    rm_n.sh

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <dir> <n>" 1>&2
+    exit 1
+fi
+
+dir="$1"
+n="$2"
+
+if [ ! -d "$dir" ]; then
+    echo "Error" 1>&2
+    exit 1
+fi
+
+find "$dir" -type f -size +"$n"c -exec rm {} \;
