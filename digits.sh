f42bf5f4aa56e8287a347c37550604d20c393924
hyzhang01 <hzhang986@wisc.edu>
2023-10-17 15:41:57 -0500

    Write two sh files.

--- digits.sh
+++ digits.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+# Initialize a variable to store the sum
+sum=0
+
+# Loop through the numbers from 1000 to 2000
+for number in {1000..2000}; do
+    # Use a regular expression to check if the number contains only 0s and 1s
+    if [[ $number =~ ^[01]+$ ]]; then
+        # If the number meets the condition, add it to the sum
+        sum=$((sum + number))
+    fi
+done
+
+# Print the sum
+echo "Sum of numbers between 1000 and 2000 with only digits 0 and 1: $sum"
--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+# Create the 'five' directory
+mkdir -p five
+
+# Loop through numbers from 1 to 5 to create subdirectories
+for i in {1..5}; do
+    subdir="five/dir$i"
+    mkdir -p "$subdir"
+    
+    # Loop through numbers from 1 to 4 to create files with appropriate content
+    for j in {1..4}; do
+        filename="$subdir/file$j"
+        
+        # Use printf to create the content for each file
+        printf "%s\n" $(seq -s "$j" "$j") > "$filename"
+    done
+done
