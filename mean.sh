eeeaa44a44e231aa2686d16d99db5413ed089b6a
ShijieChen02 <147095965+ShijieChen02@users.noreply.github.com>
2023-10-17 20:08:54 -0500

    fulfill mean.sh function

--- mean.sh
+++ mean.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+    echo "usage: ./mean.sh <column> [file.csv]" >&2
+    exit 1
+fi
+
+column=$1
+
+if [ $# -eq 1 ]; then
+    file="/dev/stdin"
+else
+    file=$2
+fi
+
+
+cut -d ',' -f "$column" "$file" | tail -n +2 | {
+    sum=0
+    count=0
+    while IFS= read -r line; do
+        sum=$(echo "$sum + $line" | bc)
+        count=$((count + 1))
+    done
+   
